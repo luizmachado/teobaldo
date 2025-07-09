67e6b3c34c77e5ebbd73d7d04a9b47bef6e90e2f
Luiz Augusto Machado da Silva <luizmachado.to@gmail.com>
2025-07-09 18:51:30 -0300

    feat(main): Implementar LLMFactory no main.py
    Implementa a classe LLMFactory no arquivo main.py para permitir a criação de diferentes modelos de linguagem grandes (LLMs) de forma mais modular e flexível.

--- src/app/main.py
+++ src/app/main.py
@@ -1,44 +1,13 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
-from dotenv import load_dotenv
-from pathlib import Path
-import getpass
-import os
+from app.models.llm_factory import LLMFactory
 
-
-# Caminho absoluto até a raiz do projeto
-env_path = Path(__file__).resolve().parents[2] / ".env"
-load_dotenv(dotenv_path=env_path)
-api_key = os.getenv("GEMINI_API_KEY")
-if not api_key:
-    import getpass
-    api_key = getpass.getpass("API Gemini:")
-
-
-# Configuraçẽos de sugurança para o modelo:
-safety_settings = {
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        }
-
-# Modelo gratuíto gemini
-model1 = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
-        safety_settings=safety_settings,
-        api_key=api_key,
-        )
+llms = LLMFactory()
+model = llms.get_model("gemini-default")
 
 
 messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to French. Translate the user sentence.",
-    ),
-    ("human", "I love programming."),
-]
-ai_msg = model1.invoke(messages)
-ai_msg
+        ("system", "Translate to French."),
+        ("human", "I love programming.")
+        ]
 
-print(ai_msg.content)
+response = model.invoke(messages)
+print(response.content)
